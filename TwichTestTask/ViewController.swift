50f15833290c9764f83564ff02a7cb22ea0f0b68
DesWolf <okuneev@gmail.com>
2020-06-30 10:58:10 +0300

    Initial Commit

---
+++ TwichTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwichTestTask
+//
+//  Created by Максим Окунеев on 6/30/20.
+//  Copyright © 2020 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
